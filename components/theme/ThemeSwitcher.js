a3938ce882659924a025ba5aa609e111f9c95989
Thada Wangthammang <mildronize@gmail.com>
2020-11-22 00:23:56 +0700

    add theme switcher

---
+++ components/theme/ThemeSwitcher.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "../../themes/Themes";
+
+import { useThemeSwitcher } from "../theme";
+
+import { GlobalStyles } from "../../themes/GlobalStyles";
+import { Normalize } from 'styled-normalize';
+
+export const ThemeSwitcher = (props) => {
+
+  const [theme, themeToggler, mountedComponent] = useThemeSwitcher();
+  if(!mountedComponent) return ( <div > {props.children}</div>)
+
+  return (
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <Normalize />
+      <GlobalStyles />
+      {props.children}
+    </ThemeProvider>
+  );
+};
+
--- pages/editor.tsx
+++ pages/editor.tsx
@@ -1,7 +1,3 @@
-import { useState } from 'react';
-import MainEditor from '../components/MainEditor/MainEditor';
-import ReactMarkdown from 'react-markdown'
-import remarkGFM from 'remark-gfm';
 import FullScreenEditor from '../components/MainEditor/FullScreenEditor';
 
 function MainPage() {
